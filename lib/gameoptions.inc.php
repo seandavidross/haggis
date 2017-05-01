0ab6848c62816936e07c5e4969c2c49d1ca20f1f
Sean Ross <seandavidross@gmail.com>
2017-05-01 09:11:28 -0700

    reformatted gameoptions.inc.php

--- lib/gameoptions.inc.php
+++ lib/gameoptions.inc.php
@@ -4,15 +4,24 @@
 ///// Game options description
 /////
 
-$game_options 
-  = array( 100 => array( 'name' => totranslate('Game duration')
-                       , 'values' => array( 2 => array( 'name' => totranslate('Long game (350 points)')
-                                                      , 'tmdisplay' => totranslate('Long game (350 points)') 
-                                                      )
-                                          , 1 => array( 'name' => totranslate('Short game (250 points)')
-                                                      , 'tmdisplay' => totranslate('Short game (250 points)') 
-                                                      )
-                                          )
-                        )
-          );
+$long_game = 
+  array( 'name' => totranslate('Long game (350 points)')
+       , 'tmdisplay' => totranslate('Long game (350 points)') 
+       );
+
+$short_game = 
+  array( 'name' => totranslate('Short game (250 points)')
+       , 'tmdisplay' => totranslate('Short game (250 points)') 
+       );
+
+$game_durations =
+  array( 2 => $long_game
+       , 1 => $short_game
+       );
+
+$game_options = 
+  array( 100 => array( 'name' => totranslate('Game duration')
+                     , 'values' => $game_durations
+                     )
+       );
 ?>
